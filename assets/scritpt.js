ded30fd3f2010e1eb16dabca678d43cd45fac66e
Big Trees <traveye@protonmail.com>
2023-01-11 00:43:01 -0800

    adding jquery, dayjs, and added currentweather function

--- assets/scritpt.js
+++ assets/scritpt.js
@@ -1,55 +1,76 @@
-const buttonEl = document.querySelector(".findCity")
+//doc selectors
+const buttonEl = document.querySelector("#click")
+const userInput = document.querySelector('#city-search')
+const currentEl = document.querySelector('#current')
 var cityList = [];
 
-function getApi() {
-    var url = "https://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=5e2060eedf6abe8e55c023dfb119a674";
+
+
+
+
+buttonEl.addEventListener("click", function(){
+    city = userInput.value
+    localStorage.setItem("city", city)
+    previosSearch()
+    getCurrent(city);
+})
+
+// this sets the previous search list with buttons
+function previosSearch() {
+    var oldCity = localStorage.getItem("city")
+    console.log(oldCity)
+    if(cityList.includes(oldCity)) {
+        console.log("repeat")
+    }
+    else {
+        cityList.push(oldCity);
+        var listEl = document.querySelector('#list')
+        var listItem = document.createElement('button')
+        listItem.textContent = oldCity
+        listItem.classList.add('list-button')
+        listEl.appendChild(listItem)
+        
+
+    }
     
-  
-    fetch(url)
-      .then(function (response) {
-        return response.json();
+}
+
+// this gets current weather
+function getCurrent(city) {
+    currentUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=5e2060eedf6abe8e55c023dfb119a674"
+    fetch(currentUrl)
+    .then(function (response) {
+        if(!response) {
+            //need text to notify user
+        }
+        else {
+            return response.json();
+            
+            
+        }
       })
-      .then(function (data) {
+    .then(function (data) {
+        console.log(data)
+        var placeName = data.name
+        var currentTime = dayjs.format('MM-DD-YYYY H:mm');
+        var displayTime = currentTime.format('MM-DD-YYYY H:mm')
+        var temp = data.main.temp
+        var wind = data.wind.speed
+        var icon = data.weather[0].icon
 
-        console.log(data);
-      
-      });
-  }
+        var placeNameRender = document.createElement("h2")
+        var iconRender = document.createElement('p')
+        var timeRender = document.createElement('h3')
+        var tempRender = document.createElement('h3')
+        var windRender = document.createElement('h3')
 
-// Search for a city: 
+        
 
-getApi();
-//need to store user input in var
+        console.log(placeName, displayTime, temp, wind, icon)
 
-// var cityInput = document.querySelector('.cityInput').value.trim();
-// localStorage.setItem("city", cityInput)
-// createList();
+      })
 
-// }
-
-// function createList() { 
+      // city name, the date, an icon representation of weather conditions, the temperature, the humidity, and the wind speed
     
-// var addCity = localStorage.getItem("city")
-// if(!cityList.includes(addCity)) {
-//     cityList.push(addCity);
-// }
-
-// console.log(cityList)
-
-// for(i = 0; i < cityList.length; i++) {
-//     searchedCity = document.querySelector('.cityList')
-//     var oldCity = document.createElement('p')
-//     oldCity.textContent = cityList[i];
-//     console.log(oldCity)
-//     searchedCity.appendChild(oldCity)
-
-// }
-
-// }
-
-// //store in local storage / add to empty var
-// //need to creante new element with var as text content
-// //append to class cityList
-
-
-//   buttonEl.addEventListener('click', getApi);
+    
+}
